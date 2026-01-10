e089459a9fe4cbb275340936f4d276d347537a4d
RathlavathRamesh <rameshrguktbasar@gmail.com>
2026-01-11 01:19:04 +0530

    done one minor change in the dataloaders to not store the tensors directly in the python list , rather return them by converting them as the tensors during the get_item method

--- Tokenizer/GptDatasetLoader.py
+++ Tokenizer/GptDatasetLoader.py
@@ -19,16 +19,18 @@ class GptDataset(Dataset):
         for i in range(0, len(token_ids) - max_length, stride):
             context = token_ids[i : i + max_length]
             target = token_ids[i + 1 : i + max_length + 1]
-            self.input_ids.append(torch.tensor(context))
+            self.input_ids.append(context)
             # print("context tensor is ",torch.tensor(context))
-            self.target_ids.append(torch.tensor(target))
+            self.target_ids.append(target)
             # print("target tensor is",torch.tensor(target))
         
     def __len__(self):
         return len(self.input_ids)
     
     def __getitem__(self,idx):
-        return self.input_ids[idx],self.target_ids[idx]
+        return (torch.tensor(self.input_ids[idx]),  
+        torch.tensor(self.target_ids[idx])
+        )
 
 def create_dataloader(txt, batch_size=4, max_length=256, stride=128, shuffle=True, drop_last=True, num_workers=0, tokenizer=None):
     dataset = GptDataset(txt, max_length, stride, tokenizer)
